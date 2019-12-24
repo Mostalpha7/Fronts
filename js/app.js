6ae71239a44ebd15debda2fda5c4b05636f29a7f
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2019-12-24 00:10:27 -0800

    Web theme by ma7- Front

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const typed = new Typed('#intro-type', {
+   strings: ['more professional', 'perfect in every ways', 'astonishing'],
+   typeSpeed: 40,
+   backSpeed: 40,
+   startDelay: 1000,
+   loop: true
+})
